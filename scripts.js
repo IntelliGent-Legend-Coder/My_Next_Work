fefb13f5762ee55dbbd6aa50aa9ba9ff75dccbff
Joydeep Das <dasjoydeepiembca2024@gmail.com>
2025-01-30 10:30:41 +0530

    thrid commit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-    // Smooth scrolling for navigation links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+       // Smooth scrolling for navigation links
+       document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
             document.querySelector(this.getAttribute('href')).scrollIntoView({
